f9dd34e383dedd55f43895e8b709e4a53ed72eb4
Nevermoi <wlgq@hotmail.com>
2023-02-13 12:23:38 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,27 +8,13 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Generate your business email"
+            content="Generate specific answer from chatGPT"
           />
           <meta
             property="og:description"
-            content="Generate your business email in seconds."
+            content="Generate specific answer from chatGPT"
           />
-          <meta property="og:title" content="Busness Email Generator" />
-          <meta
-            name="twitter:description"
-            content="Generate your business email in seconds."
-          />
-          <meta
-            property="og:image"
-            content="https://twitterbio.com/og-image.png"
-          />
-          <meta
-            name="twitter:image"
-            content="https://twitterbio.com/og-image.png"
-          />
-          {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6121888364111270"
-     crossOrigin="anonymous"></script> */}
+          <meta property="og:title" content="Answer me" />
         </Head>
         <body>
           <Main />
